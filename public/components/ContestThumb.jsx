231e93b0e582ff977fd63f10c7eba1c0fe4fded1
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2016-12-23 21:49:29 +0100

    ContestThumb ui setup

--- public/components/ContestThumb.jsx
+++ public/components/ContestThumb.jsx
@@ -9,9 +9,16 @@ module.exports = (props)=> {
         <div className="contest-thumb">
 
             <div className="box">
-                <img src={contestImage}/>
-                {name}
-                <Button />
+                <div className="row header">
+                    <div className="small-12 columns"><img src={contestImage}/></div>
+                    <div className="small-12 columns product-name">{name}</div>
+                    <div className="small-12 columns"><Button /></div>
+                </div>
+
+                <div className="row">
+                    <div className="small-6 columns">{bestTime}</div>
+                    <div className="small-6 columns">LEADERBOARD</div>
+                </div>
             </div>
 
         </div>
