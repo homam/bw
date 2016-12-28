156605c45c710f6a82b2a8e60da81336fc5346d2
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2016-12-28 15:32:13 +0100

    wrong answer shake

--- public/components/Question.jsx
+++ public/components/Question.jsx
@@ -12,14 +12,17 @@ const calculateProgress = (currentQuestion: number, totalQuestions: number): num
 
 module.exports = (props)=> {
 
-    const {optionType, options, title, questionNumber, totalQuestions, tatus} = props
+    const {optionType, options, title, questionNumber, totalQuestions, status} = props
     const {answered, isCorrect, answer} = status
 
     const progress = calculateProgress(questionNumber + 1, totalQuestions)
 
     const Options = R.map(({title})=> {
         const src = `${imagePath}${title}`
-        return (<div className="option-container" key={title}>
+
+        const shakeClass = (answered && !isCorrect && answer == title) ? "shake" : ""
+
+        return (<div className={`option-container ${shakeClass}`} key={title}>
             {/*answered && answer == title && <div className="mark correct">X</div>*/}
             <img src={src} onClick={()=> props.onAnswer(title)}/>
         </div>)
@@ -34,11 +37,11 @@ module.exports = (props)=> {
                     <div className="small-12 columns level-info">Level 1</div>
                 </div>
                 <div className="row">
-                    <div className="small-3 columns">d</div>
+                    <div className="small-3 columns">&nbsp;</div>
                     <div className="small-6 columns">
                         <ProgressBar progress={progress} />
                     </div>
-                    <div className="small-3 columns"></div>
+                    <div className="small-3 columns">&nbsp;</div>
                 </div>
                 <div className="row">
                     <div className="small-12 columns title">{title}</div>
