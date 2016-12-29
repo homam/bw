7f97ece22be58a66a2128e7d36e617f64b9b1d6e
Homam <bluesnowball@gmail.com>
2016-12-29 17:39:33 +0100

    added api-proxy for heroku

--- server.js
+++ server.js
@@ -3,9 +3,19 @@
 const express = require('express')
 const config = require('./config')
 const port = process.env.PORT || config.port
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer({changeOrigin: true});
+
+apiProxy.on('proxyRes', function (proxyRes, req, res) {
+  console.log(req)
+  console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2));
+});
 
 const app = express()
 
 app.use(express.static('./public'))
+app.all('/api/*', (req, res) => {
+  apiProxy.web(req, res, {target: 'https://prizefrenzy.com/'})
+})
 
 app.listen(port, ()=> console.log(`listening to port: ${port}`))
