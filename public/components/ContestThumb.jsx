c7cf6a97d8b54f96103eb0aae0293316f4ce0141
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2016-12-29 13:19:57 +0100

    contest thumb ui updates

--- public/components/ContestThumb.jsx
+++ public/components/ContestThumb.jsx
@@ -1,26 +1,38 @@
 const React = require('react')
+const moment = require('moment')
 const {Link} = require('react-router')
 
 const Button = require('./Button.jsx')
 
 module.exports = (props)=> {
     const {contestId, name, bestTime, contestImage, timeRemaining} = props
+    const time = moment(bestTime || 0)
 
     return (
         <div className="contest-thumb">
 
-            <div className="box">
-                <div className="row header">
-                    <div className="small-12 columns"><img src={contestImage}/></div>
-                    <div className="small-12 columns product-name">{name}</div>
-                    <div className="small-12 columns">
-                        <Link className="button" to={`/contest/${contestId}/play`}>Play Now</Link>
+            <div className="container">
+                <div className="header">
+                    <div className="row contest-info-container">
+                        <div className="small-4 columns image-container"><img src={contestImage}/></div>
+                        <div className="small-8 columns title">{name}</div>
+                    </div>
+
+                    <div className="row">
+                        <div className="small-12 columns">
+                            <Link className="button round" to={`/contest/${contestId}/play`}>Play Now</Link>
+                        </div>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="small-6 columns">{bestTime}</div>
-                    <div className="small-6 columns">LEADERBOARD</div>
+                    <div className="small-6 columns">
+                        <p>Best Time:</p>
+                        <p>{time.format('mm: ss: SSS')}</p>
+                    </div>
+                    <div className="small-6 columns">
+                        <p>LEADERBOARD</p>
+                    </div>
                 </div>
             </div>
 
