09c11ce148a2054bc4d9579975357de5666901d9
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-02 13:24:36 +0100

    Timer update: use refs instead of setting state

--- public/components/Timer.jsx
+++ public/components/Timer.jsx
@@ -2,17 +2,19 @@ const React = require('react')
 const moment = require('moment')
 const ReactTimeout = require('react-timeout')
 
+
+const formatTime = (ms: number): string => {
+    const time = moment(ms)
+    return time.format('mm: ss: SS')
+}
+
+
 module.exports = ReactTimeout(React.createClass({
 
-    render() {
+    elapsed: 0
 
-        const time = moment(this.state.elapsed)
-
-        return <div className="timer-component">{time.format('mm: ss: SS')}</div>
-    }
-
-    , getInitialState() {
-        return { elapsed: 0 }
+    , render() {
+        return <div className="timer-component" ref="timer">{formatTime(this.elapsed)}</div>
     }
 
     , componentDidMount() {
@@ -20,7 +22,9 @@ module.exports = ReactTimeout(React.createClass({
     }
 
     , tick() {
-        this.setState({elapsed: new Date() - this.props.startTime + this.props.penaltyMs})
+
+        this.elapsed = new Date() - this.props.startTime + this.props.penaltyMs
+        this.refs.timer.innerHTML = formatTime(this.elapsed)
 
         if (!this.props.pause)
             this.props.setTimeout(this.tick, 50)
