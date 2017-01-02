2989ad3c9503cc1bc29aad177a0c4b444d3d95e8
Homam <bluesnowball@gmail.com>
2017-01-02 14:37:23 +0100

    complete style updates

--- public/components/Penalty.jsx
+++ public/components/Penalty.jsx
@@ -4,11 +4,9 @@ module.exports = (props)=> {
 
     return (
         <div className="penalty-component">
-            <div className="row container">
-                <div className="small-3 columns icon">
-                    <img src="/img/icon-penalty.png" />
-                </div>
-                <div className="small-9 columns title"><p>Penalty: +1 seconds</p></div>
+            <div className="container">
+                <div className="icon" />
+                <div className="title"><p>Penalty: +1 seconds</p></div>
             </div>
         </div>
     )
