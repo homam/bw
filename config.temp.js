b6e393192b189bf425a597612c9b9a6656d5e639
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-05 15:30:10 +0100

    update config template

--- config.temp.js
+++ config.temp.js
@@ -1,4 +1,7 @@
 module.exports = {
     env: 'development'
     , port: 8000
+    , clientId : 'KTE44uAv3V'
+    , clientSecret: 'DUhMrFggWqa47mJ54m2YLLVqRJ23KRJZ'
+    , apiDomain: 'http://stage.prizefrenzy.com'
 }
