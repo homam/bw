23e36f059b9c9389150d80c8d0dde2e8769012c5
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-09 15:16:22 +0100

    number entry page ui update

--- public/components/NumberEntry.jsx
+++ public/components/NumberEntry.jsx
@@ -8,15 +8,48 @@ module.exports = React.createClass({
     }
 
     , render() {
+        const {name, contest_image} = this.props.contestItem
+
+
         return (
             <div className="number-entry-component">
+
+                <div className="row contest-info-container">
+                    <div className="contest-image" style={{backgroundImage: `url('${contest_image}')`}}>
+                        <img src="/img/icon-locked.png" />
+                    </div>
+                    <div className="info">
+                        <p className="title">{name}</p>
+                        <p className="info">Cash prize</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="separator"></div>
+                </div>
+
+                <div className="row">
+                    <h2>Enter your mobile number to unlock:</h2>
+                </div>
+
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Number:
-                        <input type="text" ref={(input) => this.input = input} />
-                    </label>
-                    <input type="submit" value="Register" />
+                    <div className="row">
+                        <input type="number" ref={(input) => this.input = input} />
+                    </div>
+                    <div className="row">
+                        <input type="submit" className="button" value="Continue" />
+                    </div>
                 </form>
+
+                <div className="row">
+                    <div className="separator"></div>
+                </div>
+
+                <div className="row">
+                    <p className="legal">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
             </div>
         )
     }
