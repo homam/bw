1c3847587db17b8c19bfed81609e3c26100a1298
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-09 15:55:37 +0100

    pin entry page ui update

--- public/components/PinEntry.jsx
+++ public/components/PinEntry.jsx
@@ -3,20 +3,54 @@ const React = require('react')
 module.exports = React.createClass({
 
     handleSubmit(event) {
-        this.props.onSubmit(this.input.value)
+        console.log(this.input.value)
+        // this.props.onSubmit(this.input.value)
         event.preventDefault();
     }
 
     , render() {
+        const {name, contest_image} = this.props.contestItem
+
         return (
             <div className="pin-entry-component">
+
+                <div className="row contest-info-container">
+                    <div className="contest-image" style={{backgroundImage: `url('${contest_image}')`}}>
+                        <img src="/img/icon-locked.png" />
+                    </div>
+                    <div className="info">
+                        <p className="title">{name}</p>
+                        <p className="info">Cash prize</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="separator"></div>
+                </div>
+
+                <div className="row pin-sent-info">
+                    <p>We have sent a 4-digit unlock code to your number.</p>
+                    <h2>Enter code:</h2>
+                </div>
+
                 <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Pin:
-                        <input type="text" ref={(input) => this.input = input} />
-                    </label>
-                    <input type="submit" value="Verify" />
+                    <div className="row">
+                        <input type="number" ref={(input) => this.input = input} />
+                    </div>
+                    <div className="row">
+                        <input type="submit" className="button" value="Unlock now" />
+                    </div>
                 </form>
+
+                <div className="row">
+                    <div className="separator"></div>
+                </div>
+
+                <div className="row">
+                    <p className="legal">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
             </div>
         )
     }
