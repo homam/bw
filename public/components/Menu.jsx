0c4540de25bb95e589cf8b5dcbb6ac4b20ed765f
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-11 15:07:12 +0100

    add profile page link in menu

--- public/components/Menu.jsx
+++ public/components/Menu.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
+const {Link} = require('react-router')
 
 module.exports = (props:{toggleMenu: () => any})=> {
 
     return (<div className="menu-component">
       <ul>
         <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Games</a></li>
-        <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Profile</a></li>
+        <li><Link className="right-off-canvas-toggle" to="/profile" onClick={props.toggleMenu}>Profile</Link></li>
         <li><a className="right-off-canvas-toggle active" href="#" onClick={props.toggleMenu}>Transactions</a></li>
         <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Your High Scores</a></li>
         <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Feedback</a></li>
