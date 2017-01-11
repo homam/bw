47bfc04e9c735c35d121112e266f16e7dd500009
Edgar Zakaryan <edgar@Edgars-MacBook-Pro.local>
2017-01-11 16:48:18 +0100

    menu redesign

--- public/components/Menu.jsx
+++ public/components/Menu.jsx
@@ -3,15 +3,39 @@ const {Link} = require('react-router')
 
 module.exports = (props:{toggleMenu: () => any})=> {
 
-    return (<div className="menu-component">
-      <ul>
-        <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Games</a></li>
-        <li><Link className="right-off-canvas-toggle" to="/profile" onClick={props.toggleMenu}>Profile</Link></li>
-        <li><a className="right-off-canvas-toggle active" href="#" onClick={props.toggleMenu}>Transactions</a></li>
-        <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Your High Scores</a></li>
-        <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Feedback</a></li>
-        <li><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>About</a></li>
-      </ul>
+    return (
+    <div className="menu-component">
+        <div className="row close-container" onClick={props.toggleMenu}>
+            <div><a className="right-off-canvas-toggle" href="#">Close</a></div>
+            <i className="icon-close"></i>
+        </div>
+
+        <div className="logo-container">
+            <img src="/img/bigwin-logo.png" />
+        </div>
+        <div className="nav">
+            <div className="row">
+                <i className="icon-game"></i>
+                <div><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Games</a></div>
+            </div>
+
+            <div className="row">
+                <i className="icon-profile"></i>
+                <div><Link className="right-off-canvas-toggle" to="/profile" onClick={props.toggleMenu}>Profile</Link></div>
+            </div>
+            <div className="row">
+                <i className="icon-transaction"></i>
+                <div><a className="right-off-canvas-toggle active" href="#" onClick={props.toggleMenu}>Transactions</a></div>
+            </div>
+            <div className="row">
+                <i className="icon-feedback"></i>
+                <div><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>Feedback</a></div>
+            </div>
+            <div className="row">
+                <i className="icon-about"></i>
+                <div><a className="right-off-canvas-toggle" href="#" onClick={props.toggleMenu}>About</a></div>
+            </div>
+        </div>
     </div>
     )
 }
